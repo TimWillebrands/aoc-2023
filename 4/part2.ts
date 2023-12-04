d14f7711a66a3720be7d72974343f4c37c804238
Tim Willebrands <mail@timwillebrands.nl>
2023-12-04 19:40:28 +0100

    idk

--- 4/part2.ts
+++ 4/part2.ts
@@ -2,7 +2,6 @@ const inputFull: string = await Deno.readTextFile(Deno.args[0]);
 
 const input = inputFull
   .split("\n")
-  .filter((line) => line !== "" && line !== " ")
   .map((line: string) => ({
     id: line.substring(line.indexOf(" "), line.indexOf(":")),
     winning: line.substring(line.indexOf(':'), line.indexOf('|')).match(/\d+/g)?.map(Number) ?? [],
@@ -11,10 +10,12 @@ const input = inputFull
   .map(line => ({
     id: Number(line.id),
     instances: 1,
-    winnings: line.numbers.reduce((wins, ownNumber) => line.winning.includes(ownNumber) ? wins+1: wins,0)
+    winnings: line.numbers
+      .reduce((wins, ownNumber) => line.winning.includes(ownNumber) ? wins+1: wins,0)
   }))
 
-const map = input.reduce((map, card) => map.set(card.id, card) , new Map<number, (typeof input)[number]>())
+const map = input
+  .reduce((map, card) => map.set(card.id, card) , new Map<number, (typeof input)[number]>())
 
 for (const card of input) {
   for(let i = card.id+1; i<= card.id+card.winnings; i++){
@@ -25,4 +26,4 @@ for (const card of input) {
   } 
 }
 
-console.log([...map.values()].reduce((score, card) => score + card.instances, 0));
+console.log(map, [...map.values()].reduce((score, card) => score + card.instances, 0));
