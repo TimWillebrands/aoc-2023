9c2fe2acd2e9a342f1ee3d0704611e146d5ef256
Tim0theus <Tim.Willebrands@gmail.com>
2023-12-05 14:31:29 +0100

    haha ok

--- 5/part1.ts
+++ 5/part1.ts
@@ -1,25 +1,26 @@
 
 const inputFull: string = await Deno.readTextFile(Deno.args[0]);
 
+function getRoute(routes:[number,number,number][], value:number){
+    const fit = routes
+        .find(route => value >= route[1] && value <= route[1]+route[2])
+        
+    return fit ? fit[0]+(value-fit[1]) : value
+}
+
 const almanac = inputFull
     .split(":")
     .slice(2)
     .map(m => m
         .split('\n')
         .map(map => map.match(/\d+/g)?.map(Number) ?? [])
-        .filter(map => map.length > 0)
-        .reduce((routes, route) => {
-            for (let i = 0; i < route[2]; i++) {
-                routes.set(route[1] + i, route[0] + i)
-            }
-            return routes
-        }, new Map<number, number>())
+        .filter(map => map.length > 0) as [number,number,number][]
     )
 
 const seeds = (inputFull
     .substring(0, inputFull.indexOf('\n'))
     .match(/\d+/g)?.map(Number) ?? [])
-    .map(seed => almanac.reduce((value, route) => route.get(value) ?? value ,seed))
+    .map(seed => almanac.reduce((value, route) => getRoute(route, value),seed))
     .sort((a,b) => a-b)
 
 console.log(seeds);
