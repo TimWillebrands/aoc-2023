dc21970e06d3041126dbba4af6a4b95be2a7503a
Tim0theus <Tim.Willebrands@gmail.com>
2023-12-07 21:44:33 +0100

    day 6

---
+++ 6/part1.ts
@@ -0,0 +1,18 @@
+const inputFull: string = await Deno.readTextFile(Deno.args[0])
+    
+const games = inputFull
+    .split('\n')
+    .map(line => Number(line.match(/\d+/g)?.join('')))
+
+const records = games[0]
+    ?.map((time, i) => [time, games[1]![i]] as [number,number])
+    .map(([time,dist]) => {
+        let wins = 0
+        for(let i=0;i<=time;i++){
+            if((time-i)*i > dist) wins++
+        }
+        return wins
+    })
+    .reduce((prev, curr) => prev*curr)
+
+console.log(games)
